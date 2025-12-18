c67d6280fb5520fcb1e8ff61b29fd364cddf66e0
Korosel <bonnetbaptiste0@gmail.com>
2025-12-18 16:59:41 +0100

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrwi9Hdox_52039OXnf2YGXXrrw_dZOic",
-  authDomain: "site-flash-card-b59c4.firebaseapp.com",
-  projectId: "site-flash-card-b59c4",
-  storageBucket: "site-flash-card-b59c4.firebasestorage.app",
-  messagingSenderId: "790096569383",
-  appId: "1:790096569383:web:15d4b44b69cc7b61f8da02",
-  measurementId: "G-KP687YWB89"
+  apiKey: "*************",
+  authDomain: "*************",
+  projectId: "************",
+  storageBucket: "************",
+  messagingSenderId: "************",
+  appId: "************",
+  measurementId: "************"
 };
 
 const app = initializeApp(firebaseConfig);
